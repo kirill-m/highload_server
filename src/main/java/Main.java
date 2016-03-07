ea4df4cdee053162a9179f8efc1d0127bcda96a0
Kirill Matveev <kmatveev15@gmail.com>
2016-03-07 19:23:22 +0300

    txt returned

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,11 +10,10 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         ServerSocket serverSocket = new ServerSocket(9000);
-        int i = 0;
-        while (i != 5) {
+
+        while (true) {
             Socket socket = serverSocket.accept();
             System.out.println("Main thread");
-            i++;
             new Thread(new Server(socket)).start();
             System.out.println("Main thread stopped");
         }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -14,8 +14,10 @@ public class Server implements Runnable {
     public void run() {
         try {
             System.out.println("from new thread");
-            readInput();
-            writeOutput();
+            String input = readInput();
+            String path = getPath(input);
+            writeOutput(path);
+            System.out.println("INPUT!!!" + input);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -28,24 +30,35 @@ public class Server implements Runnable {
         this.outputStream = socket.getOutputStream();
     }
 
-    private void readInput() throws IOException {
-        System.out.println("Someone connected to me!");
+    private String readInput() throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-        String str;
+        String str = null;
+        StringBuilder sb = new StringBuilder();
         while(true) {
             str = bufferedReader.readLine();
+            sb.append(str);
             System.out.println(str);
             if(str == null || str.length() == 0) {
                 break;
             }
         }
+        return sb.toString();
     }
 
-    private void writeOutput() throws IOException {
-        FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + "/src/static/index.html");
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fis));
-        String strLine;
+    private void writeOutput(String contentName) throws IOException {
+        String path = System.getProperty("user.dir") + "/src/static/";
+
+
+        if (contentName.length() == 1)
+            path +="index.html";
+        else
+            path += contentName;
+
+
         try {
+            FileInputStream fis = new FileInputStream(path);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(fis));
+            String strLine;
             StringBuilder sb = new StringBuilder();
 
             while((strLine = bufferedReader.readLine())!= null)
@@ -66,6 +79,28 @@ public class Server implements Runnable {
             socket.close();
         }
 
+    }
+
+    private String getPath(String s) {
+        String result = null;
+        int i = 0;
+
+        int pathIndex = "GET ".length();
+
+        for (i = pathIndex; i < s.length(); i++) {
+            if (s.charAt(i) == ' ') break;
+        }
+
+        result = s.substring(pathIndex, i);
+
+        if (result.indexOf('.') == -1){
+            if (result.charAt(result.length()-1) != '/')
+                result += "/";
+        }
+
+        System.out.println("It asks this:\n" + s);
+        System.out.println("\n" + "Work Directory:\n" + result + "\n");
+        return result;
     }
 
 }
